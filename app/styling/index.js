65b1f9adea874f57cffa9204b1f29a65f4718350
Ben Ilegbodu <ben@benmvp.com>
2017-01-19 22:02:04 -0800

    Add tests for styling helpers

--- app/styling/index.js
+++ app/styling/index.js
@@ -3,4 +3,4 @@ import {GUTTER_UNIT, GRID_COLUMN_WIDTH} from './constants'
 
 export const gutterSize = (multiplier: number): number => multiplier * GUTTER_UNIT
 
-export const gridSize = (numColumns: number): string => `${numColumns * GRID_COLUMN_WIDTH * 100}%`
+export const gridSize = (numColumns: number): string => `${(numColumns * GRID_COLUMN_WIDTH * 100).toFixed(2)}%`
