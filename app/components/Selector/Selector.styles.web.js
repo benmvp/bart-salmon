3d39069ef1c2eee93bc5166dd79f2409152dc6bf
Ben Ilegbodu <ben@benmvp.com>
2017-04-13 18:51:20 -0700

    Fixing various layout issues resulting from testing on iOS Simulator
    - Using percentage widths for `gridSize` helper for default responsiveness
    - Don’t allow for zooming in on the page (happens when interacting w/ <select>
    - Selector should clip display text w/ ellipses (was missing `overflow`)

--- app/components/Selector/Selector.styles.web.js
+++ app/components/Selector/Selector.styles.web.js
@@ -16,7 +16,8 @@ export default {
         fontSize: '20px',
         flex: '1',
         textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap'
+        whiteSpace: 'nowrap',
+        overflow: 'hidden'
     },
     arrow: {
 
