ff6d6baed093d4e21c76e248f5c404ec405eb562
Ben Ilegbodu <ben@benmvp.com>
2017-09-14 18:17:17 -0700

    Remove routing and just display routes page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,16 @@
 // @flow
-import React, {Component} from 'react'
-import {Provider} from 'react-redux'
-import {Router} from 'react-native-router-flux'
-import configureStore from '../store/configureStore'
-import scenes from './scenes'
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import configureStore from '../store/configureStore';
 
-const store = configureStore()
+import RoutesPage from './RoutesPage';
+
+const store = configureStore();
 
 export default class App extends Component {
-    render = () => (
-        <Provider store={store}>
-            <Router scenes={scenes} />
-        </Provider>
-    )
+  render = () => (
+    <Provider store={store}>
+      <RoutesPage />
+    </Provider>
+  );
 }
