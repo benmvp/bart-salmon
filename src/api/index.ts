f63306df303751273f5acf9bec1042585a3d002b
Ben Ilegbodu <ben@benmvp.com>
2019-11-22 23:36:03 -0800

    Accidentally pushed mock api replacement

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,19 +1,19 @@
-// import keyBy from 'lodash/keyBy'
-// import { fetchBartInfo } from './bart'
-// import { EtdsApiRequest } from './types'
-// import { EtdsLookup } from '../utils/types'
-
-
-// export const getEstimatedTimesOfDeparture = (): Promise<EtdsLookup> =>
-//   fetchBartInfo<EtdsApiRequest>({
-//     type: 'etd',
-//     command: 'etd',
-//     params: { orig: 'ALL' }
-//   }).then((respJson) => keyBy(respJson.station, 'abbr'))
-
+import keyBy from 'lodash/keyBy'
+import { fetchBartInfo } from './bart'
+import { EtdsApiRequest } from './types'
 import { EtdsLookup } from '../utils/types'
-import etds from './__mocks__/etds-rush-pm-off-by-one.json'
 
-export const getEstimatedTimesOfDeparture = (): Promise<EtdsLookup> => Promise.resolve(
-  (etds as unknown) as EtdsLookup
-)
+
+export const getEstimatedTimesOfDeparture = (): Promise<EtdsLookup> =>
+  fetchBartInfo<EtdsApiRequest>({
+    type: 'etd',
+    command: 'etd',
+    params: { orig: 'ALL' }
+  }).then((respJson) => keyBy(respJson.station, 'abbr'))
+
+// import { EtdsLookup } from '../utils/types'
+// import etds from './__mocks__/etds-rush-pm-off-by-one.json'
+
+// export const getEstimatedTimesOfDeparture = (): Promise<EtdsLookup> => Promise.resolve(
+//   (etds as unknown) as EtdsLookup
+// )
